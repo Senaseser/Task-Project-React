bac261a15498bb1718c3cd73371322fbb5d8f19e
Senaseser <seser0202@gmail.com>
2023-06-14 23:24:50 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const createTask = (title, taskDesc) => {
+    setTasks([
+      ...tasks,
+      { id: Math.round(Math.random() * 99999), title, taskDesc },
+    ]);
+  };
+  const deleteTaskById = (id) => {
+    const afterDeleteTask = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(afterDeleteTask);
+  };
+  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+    const updatedTask = tasks.map((task) => {
+      if (task.id === id) {
+        return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
+      }
+      return task;
+    });
+
+    setTasks(updatedTask);
+  };
+  return (
+    <div className="App">
+      <TaskCreate onCreate={createTask} />
+      <h1>Görevler</h1>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onUpdate={editTaskById}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TaskCreate.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+function TaskCreate({ task, onCreate, taskFormUpdate, onUpdate }) {
+  const [title, setTitle] = useState(task ? task.title : "");
+  const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : "");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleTaskChange = (event) => {
+    setTaskDesc(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (taskFormUpdate) {
+      onUpdate(task.id, title, taskDesc);
+    } else {
+      onCreate(title, taskDesc);
+    }
+    setTitle("");
+    setTaskDesc("");
+  };
+
+  return (
+    <div>
+      {taskFormUpdate ? (
+        <div className="taskUpdate">
+          <h3>Lütfen Taskı Düzenleyiniz!</h3>
+          <form className="taskForm">
+            <label className="label">Başlığı Düzenleyiniz</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="box"
+              type="text"
+            />
+            <label className="label">Taskı Düzenleyiniz</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="box"
+            />
+            <button onClick={handleSubmit} className="button btnUpdate">
+              Düzenle
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="taskCreate">
+          <h3>Lütfen Task Ekleyiniz!</h3>
+          <form className="taskForm">
+            <label className="label">Başlık</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="box"
+              type="text"
+            />
+            <label className="label">Task Giriniz</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="box"
+            />
+            <button onClick={handleSubmit} className="button">
+              Oluştur
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TaskCreate;
